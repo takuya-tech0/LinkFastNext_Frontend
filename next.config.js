b6bd7f747a3134b245bdd1e9d486d60c7370b308
taku <let.smakeawish1230@gmail.com>
2025-01-26 09:55:14 +0900

    useCilent環境変数/YMLデプロイ設定

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
-require('dotenv').config()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        // Reference a variable that was defined in the .env file and make it available at Build Time
-        API_ENDPOINT: process.env.API_ENDPOINT,
-        output:'standalone'
-      },
+  output: 'standalone',
+  env: {
+    API_ENDPOINT: process.env.API_ENDPOINT,
+  }
 }
 
 module.exports = nextConfig
